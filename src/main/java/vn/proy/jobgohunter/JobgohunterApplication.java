d55c58470048535010a6f5ffed7eb00c88fa183d
royhung <lemanhhung1555@gmail.com>
2026-05-24 18:18:14 +0700

    Base structure proj Job Go Hunter

---
+++ src/main/java/vn/proy/jobgohunter/JobgohunterApplication.java
@@ -0,0 +1,18 @@
+package vn.proy.jobgohunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// disable security
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class})
+
+// @SpringBootApplication
+public class JobgohunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobgohunterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/proy/jobgohunter/JobgohunterApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.proy.jobgohunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobgohunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
