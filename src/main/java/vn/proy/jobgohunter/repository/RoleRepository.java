c96587c85445ee6261de9d261439b07808406111
royhung <lemanhhung1555@gmail.com>
2026-06-07 18:08:01 +0700

    feat(permission): implement CRUD  Permissions & Roles

---
+++ src/main/java/vn/proy/jobgohunter/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package vn.proy.jobgohunter.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import vn.proy.jobgohunter.domain.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>, JpaSpecificationExecutor<Role> {
+
+    boolean existsByName(String name);
+}
