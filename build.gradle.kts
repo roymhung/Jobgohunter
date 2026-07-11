f2ff7c3e325cce63a4791562f48b9713e6eac6cd
royhung <lemanhhung1555@gmail.com>
2026-07-12 06:55:15 +0700

    feat(email): add email configuration for sending emails via Gmail with Spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
 	implementation("com.turkraft.springfilter:jpa:3.1.7")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
 
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
